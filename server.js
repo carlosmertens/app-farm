f98bd58f39cfa4d4f70c091dda62ee426c48c265
Carlos Mertens <mertenscarlos3000@gmail.com>
2024-03-16 11:08:14 +0100

    replace templates card

--- server.js
+++ server.js
@@ -3,20 +3,40 @@ const fs = require('fs');
 
 const dataJson = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const dataObj = JSON.parse(dataJson);
-console.log(dataObj);
+
+const overviewTemplate = fs.readFileSync(
+  `${__dirname}/templates/overview.html`,
+  'utf-8'
+);
+const cardTemplate = fs.readFileSync(
+  `${__dirname}/templates/card.html`,
+  'utf-8'
+);
+const productTemplate = fs.readFileSync(
+  `${__dirname}/templates/product.html`,
+  'utf-8'
+);
 
 const server = http.createServer((req, res) => {
   switch (req.url) {
     case '/':
-      res.end('OVERVIEW');
+      const cardsHtml = dataObj
+        .map(item => replaceTemplate(cardTemplate, item))
+        .join('');
+      const output = overviewTemplate.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+      res.writeHead(200, { 'Content-type': 'text/html' });
+      res.end(output);
       break;
 
     case 'overview':
-      res.end('OVERVIEW');
+      res.writeHead(200, { 'Content-type': 'text/html' });
+      res.end(overviewTemplate);
       break;
 
     case '/product':
-      res.end('PRODUCT');
+      req.writeHead(200, { 'Content-type': 'text/html' });
+      res.end(productTemplate);
       break;
 
     case '/api':
@@ -37,3 +57,18 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', (req, res) => {
   console.log('>> Server running on port 8000');
 });
+
+function replaceTemplate(template, product) {
+  let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+}
